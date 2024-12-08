5fe27a0f1aaed21ed13db5e20a57039ec3ba0668
Fausto Alexander Ramírez Ramírez <faustoramirez360@gmail.com>
2024-12-08 17:23:52 -0600

    Create P12.py

---
+++ Programas/Bloque IV/P12.py
@@ -0,0 +1,6 @@
+cadena = "Python es un lenguaje de programación"
+print("\n", cadena)
+cadena_mayusculas = cadena.upper() 
+print("\n", cadena_mayusculas)
+cadena_minusculas = cadena.lower() 
+print("\n", cadena_minusculas)
