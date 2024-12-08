af6662de4b30f3a42eb013d301dde0de427862b4
Fausto Alexander Ramírez Ramírez <faustoramirez360@gmail.com>
2024-12-08 17:41:15 -0600

    Create P5.py

---
+++ Programas/Bloque V/P5.py
@@ -0,0 +1,8 @@
+def saludar (nombre, edad):
+    print(f"¡Hola, {nombre}!")
+    print(f"Tienes {edad} años")
+    print('¡Espero que te encuentres bien!')
+
+saludar("Edgar", 26)
+saludar("PEPE", 18)
+saludar("Choto", 23)
