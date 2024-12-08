076fc49fec2e0cc5bf235ac4c5d3fde607a948d9
Fausto Alexander Ramírez Ramírez <faustoramirez360@gmail.com>
2024-12-08 17:02:44 -0600

    Create P5.py

---
+++ Programas/Bloque IV/P5.py
@@ -0,0 +1,19 @@
+letras = ["a", "b", "c", "d", "e"] 
+cont = 0
+for letra in letras:
+    cont += 1
+print("La lista \"letras\" tiene", cont, "letras")
+
+
+nums = [100, 200, 300, 400]
+sum = 0
+for num in nums:
+    sum += num
+print("la sumatoria es ", sum)
+
+
+pals = ["hoy", " ", "hace", " ", "frio"]
+con = ""
+for pal in pals:
+    con = con + pal
+print(con)
