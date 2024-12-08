1913077b2529ea787d2ebb1e858569d4ad648b43
Fausto Alexander Ramírez Ramírez <faustoramirez360@gmail.com>
2024-12-08 17:26:41 -0600

    Create P13.py

---
+++ Programas/Bloque IV/P13.py
@@ -0,0 +1,6 @@
+palabra =""
+while palabra != "salir":
+    palabra = input("Ingrese una palabra: ")
+    print("Usted ingreso: ",palabra)
+    print("Ingresa la palabra 'salir' paraterminar el programa")
+print("Fin del programa")
