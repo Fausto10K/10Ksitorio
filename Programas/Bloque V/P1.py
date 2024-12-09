0dd7346d446de3c58582e25179309acd78f1d6d8
Fausto Alexander Ramírez Ramírez <faustoramirez360@gmail.com>
2024-12-08 19:48:37 -0600

    Update P1.py

--- Programas/Bloque V/P1.py
+++ Programas/Bloque V/P1.py
@@ -1,1 +1,8 @@
+nombre = "Ediguer"
+edad =  27
+calificacion = 93.8
 
+print("Mi nombre es " + nombre + ", mi edad es " + str(edad) + ",mi calificacion es " + str(calificacion))
+print("\n")
+print(f"Nombre {nombre} , Edad {edad} , Calificacion {calificacion}")
+print("Nombre %s, edad %d, calificacion %f",(nombre,edad,calificacion))
